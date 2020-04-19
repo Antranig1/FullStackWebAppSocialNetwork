4b9c71572c537fb81436073c0be17e7e3a4216fa
antranig1 <antkevork@gmail.com>
2020-04-19 14:48:38 +0100

    Redirect to the dashboard if you are not signed in Components/dashboard/Dashboard.js && components/routing/Privateroute.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 // Redux
 import { Provider } from 'react-redux';
@@ -19,7 +21,7 @@ if (localStorage.token) {
 const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
-  },[]);
+  }, []);
   return (
     <Provider store={store}>
       <Router>
@@ -31,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
