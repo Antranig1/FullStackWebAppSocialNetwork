75727649b23cc09a3bbbbd4e081c9eabb8790459
antranig1 <antkevork@gmail.com>
2020-04-24 21:04:14 +0100

    heroku deployment

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,6 +4,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -34,6 +37,12 @@ export default function (state = initialState, action) {
         posts: state.posts.filter((post) => post._id !== payload),
         loading: false,
       };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
+        loading: false
+      };
     case POST_ERROR:
       return {
         ...state,
@@ -48,6 +57,21 @@ export default function (state = initialState, action) {
         ),
         loading: false,
       };
+      case ADD_COMMENT:
+        return{
+          ...state,
+          post: {...state.post,comments:payload},
+          loading: false
+        }
+        case REMOVE_COMMENT:
+          return{
+            ...state,
+            post: {
+              ...state.post,
+              comments: state.post.comments.filter(comment => comment._id !== payload)
+            },
+            loading:false
+          }
     default:
       return state;
   }
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 //Connect Database
 connectDB();
 
 // Init MiddleWare
-app.use(express.json({extended:false}));
-
-app.get('/',(req,res) => res.send('API Runnig'));
+app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/posts',require('./routes/api/posts'));
-app.use('/api/profile',require('./routes/api/profile'));
-app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
-const PORT = process.env.PORT || 5000;   //by default 5000
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000; //by default 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
-
