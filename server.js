38bfce54353527310c2a870c5bd7497d634b5575
Antoine <54356788+Antranig1@users.noreply.github.com>
2023-01-25 15:41:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use('/api/auth', require('./routes/api/auth'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static('client/public'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
